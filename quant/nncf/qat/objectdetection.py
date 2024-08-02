bd20d1fae56b1d6ed7d643b74bb14546f457f44e
Akash-guna <akashguna6789@gmail.com>
2024-08-02 09:20:54 +0000

    beautification NNCFQAT

--- quant/nncf/qat/objectdetection.py
+++ quant/nncf/qat/objectdetection.py
@@ -28,7 +28,7 @@ class NNCFQATObjectDetection(NNCFQAT):
         self.factor = kwargs.get("LR_SCHEDULER_FACTOR", 1)
         self.val_interval = kwargs.get("VALIDATION_INTERVAL", 1)
         self.weight_decay = kwargs.get("WEIGHT_DECAY", 0.0005)
-
+        self.work_path = os.getcwd()
     def compress_model(self):
         if (
             self.custom_model_path
@@ -50,6 +50,7 @@ class NNCFQATObjectDetection(NNCFQAT):
             self.max_box,
             self.pre_top_k,
             self.keep_top_k,
+            self.cache_path
         )
         config = build_quantization_config(
             self.ckpt_path,
@@ -64,11 +65,11 @@ class NNCFQATObjectDetection(NNCFQAT):
             self.weight_decay,
         )
         runner = customize_config(
-            config, self.data_path, self.model_path, self.batch_size
+            config, self.data_path, self.model_path, self.batch_size,self.cache_path
         )
-
+        quant_config_path = f"{self.cache_path}/current_quant_final.py"
         os.system(
-            f"python vision/core/utils/mmrazortrain.py current_quant_final.py --work-dir {self.job_path}"
+            f"python {self.work_path}vision/core/utils/mmrazortrain.py {quant_config_path} --work-dir {self.job_path}"
         )
         self.quantized_pth_location = None
         if "last_checkpoint" in os.listdir(self.model_path):
@@ -83,16 +84,18 @@ class NNCFQATObjectDetection(NNCFQAT):
             raise Exception("Fake Quantization Unsuccessful, checkpoint not found")
         self.logger.info("Fake Quantization Successful")
         # deply config
-        build_mmdeploy_config(self.imsize)
-        create_input_image(self.loaders["test"])
+        build_mmdeploy_config(self.imsize,self.cache_path)
+        create_input_image(self.loaders["test"],self.cache_path)
+        openvino_config_path = f"{self.cache_path}/current_openvino_deploy_config.py"
+        demo_img_path =f"{self.cache_path}/demo_image.png"
         os.system(
-            f"python vision/core/utils/mmrazordeploy.py current_openvino_deploy_config.py current_quant_final.py {self.quantized_pth_location} demo_image.png"
+            f"python {self.work_path}/vision/core/utils/mmrazordeploy.py {openvino_config_path} {quant_config_path} {self.quantized_pth_location} {demo_img_path}"
         )
         self.logger.info("Deployment Successful")
         shutil.move(
-            "/workspace/nyuntam/end2end.xml", os.path.join(self.model_path, "mds.xml")
+            "end2end.xml", os.path.join(self.model_path, "mds.xml")
         )
         shutil.move(
-            "/workspace/nyuntam/end2end.bin", os.path.join(self.model_path, "mds.bin")
+            "end2end.bin", os.path.join(self.model_path, "mds.bin")
         )
         return runner.model, __name__
