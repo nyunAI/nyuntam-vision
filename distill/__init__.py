5520e12eb55cde62deb4c165897104e3bc4b109a
Akash-guna <54630055+Akash-guna@users.noreply.github.com>
2024-08-05 14:19:41 +0530

    Update __init__.py

--- distill/__init__.py
+++ distill/__init__.py
@@ -5,16 +5,3 @@ def initialize_initialization(algoname):
         from .response_kd import KDTransfer
 
         return KDTransfer
-
-
-def initialize_initialization(algoname):
-    if algoname == "KDTransfer":
-        from .responsekd import (
-            Distillation,
-            KDTransferLoss,
-            RkdDistance,
-            RKdAngle,
-            KDTransfer,
-        )
-
-        return KDTransfer
